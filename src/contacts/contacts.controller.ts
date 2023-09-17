c19ffcbc44cc1ecb9e44af42fb1a9315d01facf5
vntero <shin.antero@gmail.com>
2023-09-17 21:04:12 +0200

    adds dto and imports it to the ctr

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateContactDto } from './dto/create-contact.dto';
 @Controller('contacts')
 export class ContactsController {
   @Get()
   findAll(): string {
     return 'Get all contacts';
+  }
+
+  @Post()
+  create(@Body() createContactDto: CreateContactDto): string {
+    return `Name: ${createContactDto.name}, Phone: ${createContactDto.phone}, Email: ${createContactDto.email}`;
+  }
+
+  @Put()
+  update(): string {
+    return 'Create Contact';
+  }
+
+  @Delete()
+  delete(): string {
+    return 'Create Contact';
   }
 }
---
+++ src/contacts/dto/create-contact.dto.ts
@@ -0,0 +1,5 @@
+export class CreateContactDto {
+  readonly name: string;
+  readonly phone: number;
+  readonly email: string;
+}
