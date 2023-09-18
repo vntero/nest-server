b0045f9501b67e4a7ba349a7d547dd74ed98fa56
vntero <shin.antero@gmail.com>
2023-09-18 08:42:53 +0200

    install mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsController } from './contacts/contacts.controller';
 import { ContactsService } from './contacts/contacts.service';
+import { ContactsModule } from './contacts/contacts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [ContactsModule, MongooseModule],
   controllers: [AppController, ContactsController],
   providers: [AppService, ContactsService],
 })
